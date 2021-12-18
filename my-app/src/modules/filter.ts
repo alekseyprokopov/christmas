240195e8cc34965282fb89af1d5188356f39f0f4
alProkopov <prokopov.vrn@gmail.com>
2021-12-19 00:27:28 +0300

    feat:add script for filter(except for range)

---
+++ my-app/src/modules/filter.ts
@@ -0,0 +1,26 @@
+export default function (array, config){
+    const categories = config.category
+    const parent = document.querySelector('.toy-card-container');
+
+    let filterArray = array.filter((item, index)=>{
+
+        const yearFilter = (item.dataset.year >= config.category.yearStart) && (item.dataset.year <= config.category.yearEnd)
+
+        const numberFilter = (item.dataset.number >= config.category.numberStart) && (item.dataset.number <= config.category.numberEnd)
+        console.log(config.category.favorite)
+        const favoriteFilter =  !config.category.favorite? (item.dataset.favorite === 'true'? true: false): true;
+
+        const shapeFilter = config.category.shape[item.dataset.shape]
+
+        const colorFilter = config.category.color[item.dataset.color]
+
+        const sizeFilter = config.category.size[item.dataset.size]
+
+        return yearFilter && numberFilter && shapeFilter && colorFilter && sizeFilter && favoriteFilter
+    })
+        parent!.innerHTML = '';
+        filterArray.forEach(item=> parent?.appendChild(item))
+}
+
+
+
--- my-app/src/pages/2.settings/settings.ts
+++ my-app/src/pages/2.settings/settings.ts
@@ -1,11 +1,12 @@
 import './settings.scss';
 import noUiSlider from 'nouislider';
-import {TargetElement} from 'nouislider';
+import {API, target,TargetElement} from 'nouislider';
 // // functions
 // import eventClicker from '../../modules/eventClicker';
 // import getLocalStorage from '../../modules/localStorageGet';
 // import setLocalStorage from '../../modules/localStorageSet';
 import sort from '../../modules/sort';
+import filter from '../../modules/filter';
 
 const getData = async () => {
   let url = '../../assets/data.json';
@@ -14,12 +15,42 @@ const getData = async () => {
   return data.data;
 };
 
+let config = {
+  sortSelect: 'yearPlus',
+  category:{
+    shape: {
+      'колокольчик': true,
+      'шар': true,
+      'шишка': true,
+      'снежинка': true,
+      'фигурка': true,
+    },
+    numberStart:1,
+    numberEnd:12,
+    yearStart: 1940,
+    yearEnd: 2020,
+    color:{
+      'белый':true,
+      'желтый':true,
+      'красный':true,
+      'синий':true,
+      'зелёный':true,
+    },
+    size:{
+      'большой': true,
+      'средний': true,
+      'малый': true,
+    },
+    favorite:true,
+  }
+}
+
 const Settings = {
   render: async () => {
     const toysArray = await getData()
     let toysString = toysArray.map(item=>{
       return `
-                <div class="toy-card" data-year="${item.year}" data-name="${item.name}">
+                <div class="toy-card" data-year="${item.year}" data-name="${item.name}" data-number = "${item.count}" data-year ="${item.year}" data-shape="${item.shape}" data-color = "${item.color}" data-size = "${item.size}" data-favorite ="${item.favorite}">
                     <p class="toy-card-name">${item.name}</p>
                     <div class="toy-card-image" style="background-image: url('../../assets/toys/${item.num}.png')"></div>
                     <p>Количество: <span class = 'toy-card-number'>${item.count}</span></p>
@@ -64,32 +95,27 @@ const Settings = {
                     <p class="category-title">Форма</p>
                     <div class="form-items">
                     
-                      <div class="form-item bell">
+                      <div class="form-item bell" data-name = "колокольчик">
                         <div class="form-item-picture bell-picture"></div>
                         <p class="form-item-title">Колокол</p>
                       </div>
                     
-                      <div class="form-item ball">
+                      <div class="form-item ball" data-name = "шар">
                           <div class="form-item-picture ball-picture"></div>
                           <p class="form-item-title">Шар</p>
                       </div>
                     
-                      <div class="form-item pine">
+                      <div class="form-item pine" data-name = "шишка">
                           <div class="form-item-picture pine-picture"></div>
                           <p class="form-item-title">Шишка</p>
                       </div>
                     
-                      <div class="form-item star">
-                          <div class="form-item-picture star-picture"></div>
-                          <p class="form-item-title">Звезда</p>
-                      </div>
-                    
-                      <div class="form-item snowflake">
+                      <div class="form-item snowflake" data-name = "снежинка">
                           <div class="form-item-picture snowflake-picture"></div>
                           <p class="form-item-title">Снежинка</p>
                       </div>
                     
-                      <div class="form-item figure">
+                      <div class="form-item figure" data-name = "фигурка">
                           <div class="form-item-picture figure-picture"></div>
                           <p class="form-item-title">Фигурка</p>
                       </div>
@@ -106,38 +132,38 @@ const Settings = {
                 <div class="color">
                     <p class="category-title">Цвет</p>
                     <div class="color-container">
-                        <button style="background: rgba(255, 255, 255, 1)"></button>
-                        <button style="background: rgba(253, 215, 0, 1)"></button>
-                        <button style="background: rgba(253, 0, 0, 1)"></button>
-                        <button style="background: rgba(34, 153, 235, 1)"></button>
-                        <button style="background: rgba(8, 170, 5, 1)"></button>
+                        <button class="color-item" data-color = "белый" style="background: rgba(255, 255, 255, 1)"></button>
+                        <button class="color-item" data-color = "желтый" style="background: rgba(253, 215, 0, 1)"></button>
+                        <button class="color-item" data-color = "красный" style="background: rgba(253, 0, 0, 1)"></button>
+                        <button class="color-item" data-color = "синий" style="background: rgba(34, 153, 235, 1)"></button>
+                        <button class="color-item" data-color = "зелёный" style="background: rgba(8, 170, 5, 1)"></button>
                     </div>
                 </div>
                 
                 <div class="size">
                     <p class="category-title">Размер</p>
                     <div class="size-container">
-                        <div class="size-item">
+                        <div class="size-item" data-size = "большой">
                           <input type="checkbox" id="big" name="big" checked>
-                          <label class="form-item-title" for="big">Большой</label>
+                          <label class="form-item-title" for="big" onclick="event.stopPropagation()">Большой</label>
                         </div>
                         
-                        <div class="size-item">
+                        <div class="size-item" data-size = "средний">
                           <input type="checkbox" id="middle" name="middle" checked>
-                          <label class="form-item-title" for="middle">Средний</label>
+                          <label class="form-item-title" for="middle" onclick="event.stopPropagation()">Средний</label>
                         </div>
                         
-                        <div class="size-item">
+                        <div class="size-item" data-size = "малый">
                           <input type="checkbox" id="small" name="small" checked>
-                          <label class="form-item-title" for="small">Маленький</label>
+                          <label class="form-item-title" for="small" onclick="event.stopPropagation()">Маленький</label>
                         </div>
                     </div>
                 </div>
                 
                 
-                <div class="lovely">
-                  <input type="checkbox" id="lovely" name="lovely" checked>
-                  <label class="category-title just-lovely" for="lovely">Только любимые</label>
+                <div class="favorite">
+                  <input type="checkbox" id="favorite" name="favorite" >
+                  <label class="category-title just-lovely" for="favorite" onclick="event.stopPropagation()">Только любимые</label>
                 </div>
 
             <div class="toy-config-reset">
@@ -163,10 +189,18 @@ const Settings = {
   },
   after_render: async () => {
 
-    const numberSlider= document.getElementById('number-slider')
-    const yearSlider= document.getElementById('year-slider')
 
-    noUiSlider.create(numberSlider as TargetElement, {
+
+    const numberSlider= <target>document.getElementById('number-slider')
+    const yearSlider= <target>document.getElementById('year-slider')
+
+    let bell =document.querySelector('.bell');
+    let select = document.querySelector('.sort-by') as HTMLSelectElement
+    let parent = document.querySelector('.toy-card-container');
+    let children = parent?.children
+    let defaultArray = Array.prototype.slice.call(children)
+
+    noUiSlider.create(numberSlider as target, {
       start: [1, 12],
       tooltips:true,
       connect: true,
@@ -187,7 +221,7 @@ const Settings = {
       step: 1,
     });
 
-    noUiSlider.create(yearSlider as TargetElement, {
+    noUiSlider.create(yearSlider as target, {
       start: [1940, 2020],
       tooltips:true,
       connect: true,
@@ -208,69 +242,71 @@ const Settings = {
       step: 10,
     });
 
-    let bell =document.querySelector('.bell');
-    let select = document.querySelector('.sort-by') as HTMLSelectElement
-    let parent = document.querySelector('.toy-card-container');
 
 
-    select?.addEventListener('change',()=> {
+    // (<API>numberSlider.noUiSlider).on('update',()=>{
+    //   let min = +(numberSlider as any).noUiSlider.get()[0]
+    //   let max = +(numberSlider as any).noUiSlider.get()[1]
+    //   config.category.numberStart = min
+    //   config.category.numberEnd = max
+    //   filter(defaultArray, config)
+    // })
+
+    (<API>yearSlider.noUiSlider).on('update',()=>{
+      let min = +(yearSlider as any).noUiSlider.get()[0]
+      let max = +(yearSlider as any).noUiSlider.get()[1]
+      config.category.yearStart = min
+      config.category.yearEnd = max
+      filter(defaultArray, config)
+    })
+
+  // let a = document.querySelector('.number-slider')
+  //   a?.addEventListener('mousemove',()=>console.log('hello'))
+
+
+    select.addEventListener('change',()=> {
       config.sortSelect = select?.selectedOptions[0].value
       sort(parent,config)
     })
 
-    bell?.addEventListener('click',()=> {
+    let filterItems = document?.querySelectorAll('.form-item, .color-item, .size-item, .favorite')
 
-      let children = parent?.children
-      Array.prototype.slice
-          .call(children)
-          .sort((a, b)=>a.dataset.year.localeCompare(b.dataset.year))
-          .forEach(item=> parent?.appendChild(item));
-    });
+    filterItems.forEach(item=>item.addEventListener('click',()=>{
+
+      if( item.hasAttribute('data-name')){
+        const itemName = (<HTMLElement>item).dataset.name as string;
+        config.category.shape[itemName] = !config.category.shape[itemName]
+      }
+
+      if( item.hasAttribute('data-color')) {
+        const itemColor = (<HTMLElement>item).dataset.color as string;
+        config.category.color[itemColor] = !config.category.color[itemColor]
+      }
+
+      if( item.hasAttribute('data-size')) {
+        const itemSize = (<HTMLElement>item).dataset.size as string;
+        config.category.size[itemSize] = !config.category.size[itemSize]
+      }
+
+      if (item.classList.contains('favorite')){
+        config.category.favorite = !config.category.favorite
+      }
+
+      filter(defaultArray, config)
+    }))
+
+
+
+
+
+
+
+
 
   },
 };
 
 export default Settings;
 
-// window.addEventListener('click',(e)=>{
-//   let target = e.target as HTMLInputElement
-//   if (target.classList.contains('bell-picture')){
-//     let parent = document.querySelector('.toy-card-container')
-//     console.log(parent?.children)
-//   }
-// })
 
-let config = {
-  sortSelect: 'yearPlus',
-  category:{
-    form:{
-      bell:true,
-      ball:true,
-      pine:true,
-      star:true,
-      snowflake: true,
-      figure: true
-    },
-    number:{
-      start:1,
-      end:12,
-    },
-    year:{
-      start: 1940,
-      end: 2020,
-    },
-    color:{
-      white:true,
-      yellow:true,
-      red:true,
-      blue:true,
-      green:true,
-    },
-    size:{
-      big: true,
-      middle: true,
-      small: true,
-    },
-    forward:true,
-  }
-}
+
